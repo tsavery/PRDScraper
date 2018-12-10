83f9901a123ca3f8ef6f96848d7d185cbdd1d21d
Theron Savery <savery.theron@gmail.com>
2018-12-09 23:14:44 -0500

    added npc codex scrape

--- src/scrape_monsters.py
+++ src/scrape_monsters.py
@@ -15,13 +15,17 @@ from bs4 import BeautifulSoup
 bestiary_urls = ['http://legacy.aonprd.com/bestiary/monsterIndex.html', 'http://legacy.aonprd.com/bestiary2/additionalMonsterIndex.html',
                  'http://legacy.aonprd.com/bestiary3/monsterIndex.html', 'http://legacy.aonprd.com/bestiary4/monsterIndex.html', 'http://legacy.aonprd.com/bestiary5/index.html']
 bestiary_base_urls = ['http://legacy.aonprd.com/bestiary/', 'http://legacy.aonprd.com/bestiary2/',
-                 'http://legacy.aonprd.com/bestiary3/', 'http://legacy.aonprd.com/bestiary4/', 'http://legacy.aonprd.com/bestiary5/']
+                      'http://legacy.aonprd.com/bestiary3/', 'http://legacy.aonprd.com/bestiary4/', 'http://legacy.aonprd.com/bestiary5/']
 monster_codex_url_base = 'http://legacy.aonprd.com/monsterCodex/'
 monster_codex_urls = ['boggards.html', 'bugbears.html', 'drow.html', 'duergar.html', 'fireGiants.html', 'frostGiants.html', 'ghouls.html', 'gnolls.html', 'goblins.html', 'hobgoblins.html', 'kobolds.html',
-                    'lizardfolk.html', 'ogres.html', 'orcs.html', 'ratfolk.html', 'sahuagin.html', 'serpentfolk.html', 'troglodytes.html', 'trolls.html', 'vampires.html']
+                      'lizardfolk.html', 'ogres.html', 'orcs.html', 'ratfolk.html', 'sahuagin.html', 'serpentfolk.html', 'troglodytes.html', 'trolls.html', 'vampires.html']
+npc_codex_url_base = 'http://legacy.aonprd.com/npcCodex/'
+npc_codex_urls = ['core/barbarian.html', 'core/bard.html', 'core/cleric.html', 'core/druid.html', 'core/fighter.html', 'core/monk.html', 'core/paladin.html', 'core/ranger.html',
+                  'core/rogue.html', 'core/sorcerer.html', 'core/wizard.html', 'prestige/arcaneArcher.html', 'prestige/arcaneTrickster.html', 'prestige/assassin.html',
+                  'prestige/dragonDisciple.html', 'prestige/duelist.html', 'prestige/eldritchKnight.html', 'prestige/loremaster.html', 'prestige/mysticTheurge.html',
+                  'prestige/pathfinderChronicler.html', 'prestige/shadowdancer.html', 'npc/adept.html', 'npc/aristocrat.html', 'npc/commoner.html', 'npc/expert.html', 'npc/warrior.html']
 
-
-for i in range(0,5):
+for i in range(0,0):
     print('Scraping Beastiary ' + str(i+1))
     print('====================')
     page = urllib2.urlopen(bestiary_urls[i])
@@ -146,4 +150,57 @@ utils.save_json(jsonstr, filename)
 
 jsonstr = json.dumps(spell_like_profiles, default=utils.obj_dict, indent=4)
 filename = "..\\output\\monsters\\monsterCodex_spelllikeabilities.json"
+utils.save_json(jsonstr, filename)
+
+monsters = []
+spell_profiles = []
+spell_like_profiles = []
+print("Scraping Monster Codex")
+for y in npc_codex_urls:
+    print('scraping ' + y + '...')
+    print('====================')
+    monsterpage = urllib2.urlopen(npc_codex_url_base + y)
+    monstersoup = BeautifulSoup(monsterpage, 'html.parser')
+
+    for x in monstersoup.find_all('p', class_='stat-block-title'):
+        if "CR" not in x.get_text() or "Trap" in x.get_text():
+            continue
+        name = ""
+        cr = 0
+        if x.b and x.b.find('span', class_='stat-block-cr') or x.find('span', class_='stat-block-cr'):
+            if(x.b):
+                pair = x.b.get_text().split('CR')
+            else:
+                pair = x.get_text().split('CR')
+            name = utils.remove_trailing_and_leading_spaces(pair[0])
+
+            cr = utils.remove_trailing_and_leading_spaces(pair[1])
+
+            currentElement = x
+
+            # get all the content for the stat block as plain text
+            buffer = ""
+            lines = 0
+            while currentElement.get('class'):
+                if 'stat-block-title' in currentElement.get('class') and lines > 0:
+                    break
+                currentElement = currentElement.find_next('p')
+                buffer += currentElement.get_text() + "\n"
+                lines += 1
+            if lines < 5:
+                continue
+            print('\t' + name.encode('utf-8') + ' CR ' + cr.encode('utf-8'))
+            scraper.scrape_monster(buffer, name, cr, monsters, spell_profiles, spell_like_profiles, "Pathfinder RPG Monster Codex")
+
+# Dump Data to Json File
+jsonstr = json.dumps(monsters, default=utils.obj_dict, indent=4)
+filename = "..\\output\\monsters\\npcCodex.json"
+utils.save_json(jsonstr, filename)
+
+jsonstr = json.dumps(spell_profiles, default=utils.obj_dict, indent=4)
+filename = "..\\output\\monsters\\npcCodex_spells.json"
+utils.save_json(jsonstr, filename)
+
+jsonstr = json.dumps(spell_like_profiles, default=utils.obj_dict, indent=4)
+filename = "..\\output\\monsters\\npcCodex_spelllikeabilities.json"
 utils.save_json(jsonstr, filename)
--- src/scraper.py
+++ src/scraper.py
@@ -353,12 +353,16 @@ def scrape_monster(buffer, name, cr, monsters, spell_profiles, spell_like_profil
         monster.type = result
 
     # Classline
-    regex = re.compile('(?<=\n)[A-Z][a-z]+ ([a-z/\(\)\d]+ )+\d{1,2}(?=\n)')
-    match = regex.search(buffer)
-    if match:
-        result = match.group()
-        if 'speed' not in result and 'Resist' not in result:
-            monster.classline = result
+    regex = re.compile('(?<=\n)[A-Z][a-z\-]+(?:[a-z/\(\)\d\- ]* )+\d{1,2}(?=\n)')
+    matches = regex.findall(buffer)
+    if len(matches) is 1:
+        result = matches[0]
+        monster.classline = result
+    elif len(matches) > 1:
+        result = matches[0]
+        for s in range(1, len(matches)):
+            result += '/' + matches[s].lower()
+        monster.classline = result
 
     #Encode the buffer for spellcasting checks
     buffer = buffer.encode('utf-8')
